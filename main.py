dfa7ae55ace430e631e145231147f46c2cec8d6d
desmondfong <68687290+desmondfong@users.noreply.github.com>
2021-03-27 20:57:37 +0800

    MakeCode 專案的初始檔案

---
+++ main.py
@@ -0,0 +1,2 @@
+basic.show_string("Hello!")
+basic.show_string("Press A to know the co2 and press B to know the temperature and press A+B to humidity.The lights will show you the weather is it go to go out.")
---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (COZIR.Co2()))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (input.temperature()))
+})
+COZIR.calibrateCo2()
+let strip = neopixel.create(DigitalPin.P3, 24, NeoPixelMode.RGB)
+basic.showString("Hello!")
+basic.showString("Press A to know the co2 and press B to know the temperature.The lights will show you the weather is it go to go out.")
+radio.setGroup(1)
+basic.forever(function () {
+    radio.setGroup(1)
+    radio.sendNumber(COZIR.Co2())
+})
+basic.forever(function () {
+    radio.setGroup(1)
+    if (35 < 35) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    } else if (0 < 54) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    } else {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    }
+})
